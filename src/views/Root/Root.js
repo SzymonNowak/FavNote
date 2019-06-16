1a6f9cf37a206f93e6a13a311c53e781ab34b292
unknown <nnja5@o2.pl>
2019-06-17 00:39:14 +0200

    add ThemeProvider

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -2,14 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 import Button from '../../components/atoms/Button/Button';
 import GlobalStyle from '../../theme/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
 
 
+const theme = {
+  primary: 'black',
+}
 
 const Root = () => (
   <div className="App">
     <GlobalStyle/>
-    <Button width=""> Close/Save</Button>
-    <Button secondary >Remove</Button>
+    <ThemeProvider theme={theme}>
+    <>
+        <Button width=""> Close/Save</Button>
+        <Button secondary >Remove</Button>
+    </>
+    </ThemeProvider>
 
   </div>
 );
