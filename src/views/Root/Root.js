9a2ba3e3b219adf914aa022911bf73bcae542a9a
unknown <nnja5@o2.pl>
2019-06-16 14:18:48 +0200

    start of FavNote

---
+++ src/views/Root/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../../components/Button/Button';
+
+
+const Root = () => (
+  <div className="App">
+    <Button width="500px"> Close/Save</Button>
+    <Button secondary >Remove</Button>
+
+  </div>
+);
+
+export default Root;
