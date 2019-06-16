bf167363403a35a2e250062eaaa121af4f1e5b2d
unknown <nnja5@o2.pl>
2019-06-17 00:48:40 +0200

    create mainTheme

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -3,20 +3,16 @@ import styled from 'styled-components';
 import Button from '../../components/atoms/Button/Button';
 import GlobalStyle from '../../theme/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
-
-
-const theme = {
-  primary: 'black',
-}
+import { theme } from '../../theme/maintheme';
 
 const Root = () => (
   <div className="App">
     <GlobalStyle/>
-    <ThemeProvider theme={theme}>
-    <>
-        <Button width=""> Close/Save</Button>
-        <Button secondary >Remove</Button>
-    </>
+    <ThemeProvider theme={theme}> 
+      <>
+          <Button width=""> Close/Save</Button>
+          <Button secondary >Remove</Button>
+      </>
     </ThemeProvider>
 
   </div>
